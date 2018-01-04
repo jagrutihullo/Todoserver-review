30bda38d4eba4715897361b6289da061194bb6be
Jagruti Rasal <jagrutirasal@Jagrutis-MacBook-Air.local>
2018-01-04 17:30:22 +0530

    after negative tests

--- *_test.go
+++ *_test.go
@@ -27,7 +27,6 @@ func TestFetchAll(t *testing.T) {
 }
 
 func TestFetch(t *testing.T) {
-	//Positive
 	list = List{
 		ID: 1,
 	}
@@ -38,43 +37,17 @@ func TestFetch(t *testing.T) {
 	} else {
 		fmt.Println(list)
 	}
-
-	//Negative
-	list = List{
-		ID: 0,
-	}
-	list, err = gormRepo.Fetch(list)
-	if err.Error() != "" {
-		t.Errorf("List not fetched, got %s, want list with ID %d "+
-			"and all tasks under it", err, list.ID)
-	} else {
-		fmt.Println(list)
-	}
 }
 
 func TestCreate(t *testing.T) {
 	list = List{
-		Name: "demo list",
+		Name: "demo list 1",
 	}
 	err = gormRepo.Create(list)
 	if err.Error() != "" {
 		t.Errorf("List not created, got %s, want no error", err)
 	} else {
-		fmt.Println("Created list is ")
-		//list, err = gormRepo.Fetch(list)
-		//fmt.Println(list)
-	}
-
-	list = List{
-		Name: "demo list",
-	}
-	err = gormRepo.Create(list)
-	if err.Error() != "" {
-		t.Errorf("List not created, got %s, want no error", err)
-	} else {
-		fmt.Println("Created list is ")
-		//list, err = gormRepo.Fetch(list)
-		//fmt.Println(list)
+		fmt.Println("List Created")
 	}
 }
 
@@ -91,35 +64,11 @@ func TestUpdate(t *testing.T) {
 		list, err = gormRepo.Fetch(list)
 		fmt.Println(list)
 	}
-
-	list = List{
-		ID:   0,
-		Name: "demo list 2",
-	}
-	err = gormRepo.Update(list)
-	if err.Error() != "" {
-		t.Errorf("List not updated, got %s, want updated Name = %s", err, list.Name)
-	} else {
-		fmt.Println("Updated list is ")
-		list, err = gormRepo.Fetch(list)
-		fmt.Println(list)
-	}
 }
 
 func TestDelete(t *testing.T) {
 	list = List{
 		ID: 1,
-	}
-	err = gormRepo.Delete(list)
-	if err.Error() != "" {
-		t.Errorf("List not deleted, got %s, want no error", err)
-	} else {
-		list, err = gormRepo.Fetch(list)
-		fmt.Println("Correct Output after deletion: ", err)
-	}
-
-	list = List{
-		ID: 2,
 	}
 	err = gormRepo.Delete(list)
 	if err.Error() != "" {
@@ -145,27 +94,7 @@ func TestCreateTask(t *testing.T) {
 	if err.Error() != "" {
 		t.Errorf("Task not created, got %s, want no error", err)
 	} else {
-		fmt.Println("Created Task is ")
-		//list, err = gormRepo.FetchTask(list)
-		//fmt.Println(list)
-	}
-
-	list = List{
-		ID: 6,
-		Tasks: []Task{{
-			Description: "task1 list6",
-			Status:      "pending",
-			Deadline:    time.Date(2018, 3, 15, 00, 00, 00, 00, time.UTC),
-		},
-		},
-	}
-	err = gormRepo.CreateTask(list)
-	if err.Error() != "" {
-		t.Errorf("Task not created, got %s, want no error", err)
-	} else {
-		fmt.Println("Created Task is ")
-		//list, err = gormRepo.FetchTask(list)
-		//fmt.Println(list)
+		fmt.Println("Task Created")
 	}
 }
 
@@ -173,25 +102,6 @@ func TestUpdateTask(t *testing.T) {
 	list = List{
 		Tasks: []Task{{
 			ID:          1,
-			Description: "todoserver testing",
-			Status:      "pending",
-			Deadline:    time.Date(2018, 3, 15, 00, 00, 00, 00, time.UTC),
-		},
-		},
-	}
-	err = gormRepo.UpdateTask(list)
-	if err.Error() != "" {
-		t.Errorf("Task not updated, got %s", err)
-		t.Error("want updated task as ", list.Tasks[0])
-	} else {
-		fmt.Println("Updated task is ")
-		list, err = gormRepo.FetchTask(list)
-		fmt.Println(list)
-	}
-
-	list = List{
-		Tasks: []Task{{
-			ID:          0,
 			Description: "todoserver testing",
 			Status:      "pending",
 			Deadline:    time.Date(2018, 3, 15, 00, 00, 00, 00, time.UTC),
@@ -223,20 +133,6 @@ func TestDeleteTask(t *testing.T) {
 		list, err = gormRepo.FetchTask(list)
 		fmt.Println("Correct Output after deletion: ", err)
 	}
-
-	list = List{
-		Tasks: []Task{{
-			ID: 1,
-		},
-		},
-	}
-	err = gormRepo.DeleteTask(list)
-	if err.Error() != "" {
-		t.Errorf("Task not deleted, got %s, want no error", err)
-	} else {
-		list, err = gormRepo.FetchTask(list)
-		fmt.Println("Correct Output after deletion: ", err)
-	}
 }
 
 func TestFetchTask(t *testing.T) {
@@ -247,23 +143,8 @@ func TestFetchTask(t *testing.T) {
 		},
 	}
 	list, err = gormRepo.FetchTask(list)
-	id := list.Tasks[0].ID
 	if err != nil {
 		t.Errorf("Task not fetched, got %s, want task with ID %d", err, list.Tasks[0].ID)
-	} else {
-		fmt.Println(list)
-	}
-
-	list = List{
-		Tasks: []Task{{
-			ID: 0,
-		},
-		},
-	}
-	id = list.Tasks[0].ID
-	list, err = gormRepo.FetchTask(list)
-	if err.Error() != "" {
-		t.Errorf("Task not fetched, got %s, want task with ID %d", err, id)
 	} else {
 		fmt.Println(list)
 	}
